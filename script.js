6ffc7afcd79bd8376a89af4d409a33d8dbc60a43
Abdul Mosin <abdulmosin178@gmail.com>
2025-11-30 08:36:26 +0530

    3rd commit

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ document.addEventListener("DOMContentLoaded", function () {
     yearSpan.textContent = new Date().getFullYear();
   }
 });
+function sendToWhatsApp(e) {
+  e.preventDefault(); // stop page refresh
+
+  let name = document.querySelector('input[placeholder="Your Name"]').value;
+  let email = document.querySelector('input[placeholder="Your Email"]').value;
+  let country = document.querySelector('input[placeholder="Your Country"]').value;
+  let message = document.querySelector('textarea').value;
+
+  let whatsappNumber = "917305153682"; // your WhatsApp number
+
+  let text =
+    "📩 *New Export Enquiry*\n\n" +
+    "*Name:* " + name + "\n" +
+    "*Email:* " + email + "\n" +
+    "*Country:* " + country + "\n" +
+    "*Message:* " + message;
+
+  let url = "https://wa.me/" + whatsappNumber + "?text=" + encodeURIComponent(text);
+
+  window.open(url, "_blank"); // open WhatsApp
+}
